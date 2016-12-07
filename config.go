46d64a9a7f31298e3dc521b82721ced489346251
Jane Prather <jane@prather.io>
2016-12-07 11:59:47 -0800

    add basic config validation

--- config.go
+++ config.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"io/ioutil"
 	"log"
+	"regexp"
 	"time"
 
 	yaml "gopkg.in/yaml.v2"
@@ -23,7 +24,6 @@ type configClass struct {
 		Password string
 	}
 	TimeoutLogin time.Duration
-	TimeoutData  time.Duration
 }
 
 func initConfig() {
@@ -37,5 +37,67 @@ func initConfig() {
 	err = yaml.Unmarshal(data, config)
 	if err != nil {
 		log.Fatalf("Unable to parse config: %s", err.Error())
+	}
+
+	validateConfig()
+}
+
+func validateConfig() {
+	hasErrors := false
+
+	// ensure that bindaddress is a valid pattern
+	if match, err := regexp.MatchString("(i?)^[0-9a-z\\-\\_\\.]*:[0-9]+$",
+		config.BindAddress); err != nil || !match {
+		log.Println("config: bindaddress must be in \"host:port\" or \":port\" " +
+			"format")
+		hasErrors = true
+	}
+
+	// ensure that prometheus metric namespace is reasonable
+	if match, err := regexp.MatchString("(i?)^[a-z0-9]+$",
+		config.NameSpace); err != nil || !match {
+		log.Println("config: namespace must be reasonable metrics namespace " +
+			"(try \"xen\")")
+		hasErrors = true
+	}
+
+	// ensure that username has been specified
+	if config.Auth.Username == "" {
+		log.Println("config: auth/username must be specified")
+		hasErrors = true
+	}
+
+	// ensure that password has been specified
+	if config.Auth.Password == "" {
+		log.Println("config: auth/password must be specified")
+		hasErrors = true
+	}
+
+	// ensure that a timeout value isn't missing/zero
+	if config.TimeoutLogin == 0 {
+		log.Println("config: timeoutlogin should be non-zero")
+		hasErrors = true
+	}
+
+	// ensure at least one pool is configured from which to gather data
+	if len(config.Pools) == 0 {
+		log.Println("config: no pools configured")
+		hasErrors = true
+	}
+
+	// if enabledmetrics is specified, ensure only valid metrics are listed
+	if len(config.EnabledMetrics) > 0 {
+		for _, mName := range config.EnabledMetrics {
+			if _, ok := metricHelp[mName]; !ok {
+				log.Printf("config: enabledmetrics contains invalid metric name: %s\n",
+					mName)
+				hasErrors = true
+			}
+		}
+	}
+
+	// if any errors occurred, abort the program
+	if hasErrors {
+		log.Fatalln("Aborting due to configuration errors.")
 	}
 }
